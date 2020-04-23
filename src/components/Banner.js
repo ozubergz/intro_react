a198c092af239c425a6ef8c21d353e97e3102af3
ozubergz <jmin499@gmail.com>
2020-04-23 16:48:39 -0400

    removed classname banner_logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,8 @@ import logo from '../images/ff7_logo.png';
 class Banner extends Component {
     render() {
         return(
-            <div className="banner_container">
-                <img src={logo} className="banner_logo" alt="FF7 Remake Logo" />
+            <div className="banner">
+                <img src={logo} alt="FF7 Remake Logo" />
             </div>
         )
     }
